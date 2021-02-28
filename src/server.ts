6f4a845862eb932a37f8f4052e4aa45a8994d792
kakacordovil <kakacordovil@gmail.com>
2021-02-28 03:09:27 +0000

    survey create and survey show methods

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
+
 import './database';
+import { router } from './routes';
 
 const app = express();
 
-
-
-app.get("/" , (request, response) => {
-    return response.json({ message: "Hello world nlw"});
-});
-
-app.post("/" , (request, response) => {
-    return response.json({ message: "data saved"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, ()=> console.log("Server is running"));
